59a6c20a00cece32011e6bad722dd663267f07f3
Dani Mangtani Madani <DaniMangtani@macbook-air-de-dani.local>
2016-08-15 13:46:32 +0100

    Initial Commit

---
+++ Coursera0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coursera0
+//
+//  Created by Dani Mangtani Madani on 15/8/16.
+//  Copyright © 2016 Dani Mangtani Madani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
